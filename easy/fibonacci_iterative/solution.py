8a67ae61d731fc401a2d5b3bfb6238ad31eb7778
Shubham Bhardwaj <sbmbhardwaj01@gmail.com>
2022-10-14 01:23:17 +0530

    solution for fibonacci iterative

--- easy/fibonacci_iterative/solution.py
+++ easy/fibonacci_iterative/solution.py
@@ -1,12 +1,15 @@
-def answer(n):
-    a=0
-    b=1
-    c=0
-    for i in range(1,n+1):
-        c=a+b
-        a=b
-        b=c
-    print(a)
-n=int(input())
-answer(n)
+def solution(*args):
+    if len(args)==0:
+        return -1
+    if args[0]>=0:
+
+        a=0
+        b=1
+        c=0
+        for i in range(1,args[0]+1):
+            c=a+b
+            a=b
+            b=c
+        return a
+    return -1
 
